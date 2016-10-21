7135872b6d5e6b12ddb24be02ee5b192e6f615e0
苏佳 <gslzsj888@163.com>
2016-10-21 18:50:15 +0800

    '工具review'

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,26 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
+	public static void main(final String[] args) throws NotPolynomialException {
 		final String end = "quit";
 		Polynomial polynomial = new Polynomial();
 		while (true) {
 			System.out.print(">");
-			Scanner s = new Scanner(System.in);
-			String line = s.nextLine();
+			final Scanner s = new Scanner(System.in);
+			final String line = s.nextLine();
 			if (line.equals(end)) {
 				break;
 			}
-
+			int len = line.length();
 			if (line.matches("^!simplify.*")) {
-				String ord1 = line.substring(10);
-				System.out.println(polynomial.simplify(ord1));
+				if (len <= 10) {
+					System.out.println(polynomial.simplify(""));
+					continue;
+				}
+				final String ord1 = line.substring(10);
+				String temp = polynomial.simplify(ord1);
+				System.out.println(temp);
+				polynomial = Parse.string2Polynomial(temp);
 			} else if (line.matches("^!d/d.*")) {
-				String ord2 = line.substring(4);
-				System.out.println(polynomial.derivative(ord2));
+				if (len <= 4) {
+					System.out.println("No such var");	
+					continue;
+				}
+				final String ord2 = line.substring(4);
+				String temp = polynomial.derivative(ord2);
+				System.out.println(temp);
+				polynomial = Parse.string2Polynomial(temp);
 			} else {
 				try {
-					polynomial = Parse.String2Polynomial(line);
+					polynomial = Parse.string2Polynomial(line);
 					System.out.println(polynomial.toString());
 				} catch (NotPolynomialException e) {
 					System.out.println("This is not a polynomial.");
